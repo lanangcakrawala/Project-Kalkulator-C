cf92348cc50915e1bf316e1e4d450689e9bc1be5
lanangcakrawala <lanangcakrawalat@gmail.com>
2026-05-02 22:14:47 +0700

    Cakra: Implementasi logika perkalian dan menu input

--- main.c
+++ main.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 
-    // --- DEKLARASI FUNGSI ---
-    // (Masing-masing anggota nanti menambahkan fungsinya di sini)
+// Prototipe Fungsi (Sudah kamu tambahkan di commit 1)
+float kali(float a, float b);
 
-    int main() {
-        int pilihan;
-        float num1, num2, hasil;
+int main() {
+    float num1, num2, hasil;
+    int pilihan;
 
-        printf("=== KALKULATOR KELOMPOK ===\n");
-        printf("1. Tambah\n2. Kurang\n3. Kali\n4. Bagi\n");
-        printf("Pilih operasi: ");
-        scanf("%d", &pilihan);
+    printf("=== KALKULATOR KELOMPOK ===\n");
+    printf("1. Perkalian\n");
+    printf("Pilih operasi: ");
+    scanf("%d", &pilihan);
 
-        // Nanti logic switch-case atau pemanggilan fungsi di bawah sini
-        
-        return 0;
+    printf("Masukkan angka pertama: ");
+    scanf("%f", &num1);
+    printf("Masukkan angka kedua: ");
+    scanf("%f", &num2);
+
+    if(pilihan == 1) {
+        hasil = kali(num1, num2);
+        printf("Hasil perkalian: %.2f\n", hasil);
     }
+
+    return 0;
+}
+
+// Logika Fungsi (Tahap ini yang ditambahkan sekarang)
+float kali(float a, float b) {
+    return a * b;
+}
