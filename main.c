1e3d91f2365d87e61809eb22888753d1dbb5a138
ivannvv <ivan.nuryadin@ui.ac.id>
2026-05-02 20:55:13 +0700

    Sentuhan terakhir

--- main.c
+++ main.c
@@ -9,14 +9,21 @@ int main() {
     int choice;
     float n1, n2;
 
+    printf("Select (1:Add, 2:Sub, 3:Mul, 4:Div): ");
     scanf("%d", &choice);
+    printf("Input 2 numbers: ");
     scanf("%f %f", &n1, &n2);
 
+    printf("Result: ");
     if (choice == 1) printf("%.2f", add(n1, n2));
     else if (choice == 2) printf("%.2f", sub(n1, n2));
     else if (choice == 3) printf("%.2f", mul(n1, n2));
-    else if (choice == 4 && n2 != 0) printf("%.2f", div(n1, n2));
-
+    else if (choice == 4) {
+        if (n2 != 0) printf("%.2f", div(n1, n2));
+        else printf("Math Error");
+    }
+    
+    printf("\n");
     return 0;
 }
 
