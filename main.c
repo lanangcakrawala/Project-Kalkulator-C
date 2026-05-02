8afd6120ddc7a4cbc1a43bc33c45f7cf2b5307ac
lanangcakrawala <lanangcakrawalat@gmail.com>
2026-05-02 22:09:17 +0700

    Cakra: Menambahkan prototipe fungsi perkalian

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+    // --- DEKLARASI FUNGSI ---
+    // (Masing-masing anggota nanti menambahkan fungsinya di sini)
+
+    int main() {
+        int pilihan;
+        float num1, num2, hasil;
+
+        printf("=== KALKULATOR KELOMPOK ===\n");
+        printf("1. Tambah\n2. Kurang\n3. Kali\n4. Bagi\n");
+        printf("Pilih operasi: ");
+        scanf("%d", &pilihan);
+
+        // Nanti logic switch-case atau pemanggilan fungsi di bawah sini
+        
+        return 0;
+    }
